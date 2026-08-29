b01481dc91d42c4e9b2b710cbaef5e886bac833a
iki1uc <entertrainer@web.de>
2026-08-29 23:03:54 +0200

    Create 3.js

---
+++ tmp/3.js
@@ -0,0 +1,21 @@
+class PrefetchPump {
+  constructor() {
+    this.level = "L1";      // L1, L2, L3, AMP
+    this.mode  = "linear";  // linear, page, pointer, adaptive
+    this.force = 1.0;       // Pump‑Stärke
+  }
+
+  pump(address) {
+    // Hardware‑nahe Prefetch‑Steuerung
+    _mm_prefetch(address, this._flag());
+  }
+
+  _flag() {
+    switch(this.mode) {
+      case "linear":   return _MM_HINT_T0;   // L1 NLP
+      case "page":     return _MM_HINT_T1;   // L1 NPP
+      case "pointer":  return _MM_HINT_T2;   // AOPP
+      case "adaptive": return _MM_HINT_NTA;  // AMP
+    }
+  }
+}
